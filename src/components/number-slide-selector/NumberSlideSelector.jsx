5ff133d439ffcae7bf325007987bc317f9c910aa
AlanLee <1445654576@qq.com>
2023-04-25 00:56:16 +0800

    fix: 调整滑动选择效果

--- src/components/number-slide-selector/NumberSlideSelector.jsx
+++ src/components/number-slide-selector/NumberSlideSelector.jsx
@@ -1,11 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.scss';
 
 export default function NumberSlideSelector(props = {}) {
-  const {start = 0, end = 23, width = 100, unit = ''} = props;
+  const {start = 0, end = 23, width = 100, unit = '', onSelected = () => {}} = props;
   const [list, setList] = useState([]);
   const [reachEnd, setReachEnd] = useState(true);
+  const [scrollFinsh, setScrollFinsh] = useState(true);
   const [curIndex, setCurIndex] = useState(0);
+  let [scrollTimer, setScrollTimer] = useState(null);
+  let [tapping, setTapping] = useState(false);
+  const [disableOnScroll, setDisableOnScroll] = useState(false);
+  const slideWrapperRef = useRef()
+
 
   const genarateList = () => {
     for(let i = start; i <= end; i++) {
@@ -14,28 +20,51 @@ export default function NumberSlideSelector(props = {}) {
   }
 
   const onScroll = (e) => {
+    if(disableOnScroll) return;
     let curIndex = Math.ceil((e.target.scrollTop / 60) + 2)
-    // console.log('onScroll', curIndex);
     setCurIndex(curIndex);
     setReachEnd(e.target.scrollTop + 300 === e.target.scrollHeight)
+
+    if(scrollTimer) {
+      clearTimeout(scrollTimer)
+      setScrollFinsh(false);
+    }
+    setScrollTimer(setTimeout(() => {
+      setScrollFinsh(true);
+      if(!tapping) {
+        setDisableOnScroll(true);
+        e.target.scrollTop = curIndex * 60 - 120
+        setTimeout(()=> {
+          setDisableOnScroll(false);
+          onSelected(e.target.querySelectorAll('.slide-item-active')[0].textContent)
+        }, 100)
+      }
+    }, 100))
   }
 
-  useEffect(() => {
-    console.log('useEffect-1');
-    // console.log(list);
-  })
+  const onTouchStart = e => {
+    setTapping(true);
+  }
+
+  const onTouchEnd = e => {
+    setTapping(false);
+    if(disableOnScroll) return;
+    let el = slideWrapperRef.current
+
+    setDisableOnScroll(true);
+    el.scrollTop = curIndex * 60 - 120
+    setTimeout(()=> {
+      setDisableOnScroll(false);
+    }, 100)
+  }
 
   useEffect(() => {
     list.unshift(String(end - 0));
     list.unshift(String(end - 1));
     setList(list);
-    console.log('useEffect-2');
     setCurIndex(2)
+    console.log('mounted');
   }, [])
-
-  useEffect(() => {
-    console.log('useEffect-3');
-  }, [list])
 
   if(reachEnd) {
     genarateList();
@@ -44,10 +73,11 @@ export default function NumberSlideSelector(props = {}) {
 
   return (
     <div className="cpn--number-slide-selector" style={{width: `${width}px`}}>
-      <div className='number-slide-wrapper' onScroll={onScroll}>
+      <div className='number-slide-wrapper' ref={slideWrapperRef} onScroll={onScroll} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
         {
           list.map((item, i) => (
             <div 
+            data-i={i}
             className={['slide-item', 
             curIndex === i ? 'slide-item-active' : '', 
             (curIndex - 1 === i || curIndex + 1 === i) ? 'slide-item-second' : ''].join(' ').trim()} 
--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -3,18 +3,31 @@ import NumberSlideSelector from "../components/number-slide-selector/NumberSlide
 import './style.scss';
 
 export default class TimerPage extends React.Component {
+
+  onSelectedHour = val => {
+    console.log('onSelectedHour', val);
+  }
+
+  onSelectedMin = val => {
+    console.log('onSelectedMin', val);
+  }
+
+  onSelectedSec = val => {
+    console.log('onSelectedSec', val);
+  }
+
   render() {
     return (
       <div className="page--timer">
         <section className="time-selector-wrapper">
           <div className="single-time-selector-wrapper">
-            <NumberSlideSelector start={0} end={23} unit={'时'} />
+            <NumberSlideSelector start={0} end={23} unit={'时'} onSelected={this.onSelectedHour} />
           </div>
           <div className="single-time-selector-wrapper middle-item">
-            <NumberSlideSelector start={0} end={59} unit={'分'} />
+            <NumberSlideSelector start={0} end={59} unit={'分'} onSelected={this.onSelectedMin} />
           </div>
           <div className="single-time-selector-wrapper">
-            <NumberSlideSelector start={0} end={59} unit={'秒'} />
+            <NumberSlideSelector start={0} end={59} unit={'秒'} onSelected={this.onSelectedSec} />
           </div>
         </section>
       </div>
