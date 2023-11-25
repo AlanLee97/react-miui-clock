ac28ec023e9c53d5a2d115d4267da57d501f2596
AlanLee <1445654576@qq.com>
2023-11-25 22:50:30 +0800

    fix: 配置项拆分

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const enableAnalyzer = (process.argv || []).includes('analyzer');
+
+const baseConfig = {
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  }
+};
+
+const prodConfig = {
+  plugins: [
+    new CompressionPlugin(),
+    enableAnalyzer ? new BundleAnalyzerPlugin() : undefined
+  ]
+};
+
+const devConfig = {
+  
+};
 
 module.exports = function override(config, env) {
   const prodEnv = env === 'production';
-  const customConfig = {
-    externals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-    },
-    plugins: [
-      prodEnv ? new CompressionPlugin() : undefined
-    ]
-  };
-  const mregedConfig = merge(config, customConfig);
+  const mregedConfig = merge(config, baseConfig, prodEnv ? prodConfig : devConfig);
   return mregedConfig;
 };
